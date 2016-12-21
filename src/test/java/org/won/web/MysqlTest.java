64641941d4e66928c41eae7bca4aa0440e4ed99a
Administrator <Administrator@2bmw3>
2016-12-21 10:30:17 +0900

    hi hi

---
+++ src/test/java/org/won/web/MysqlTest.java
@@ -0,0 +1,38 @@
+package org.won.web;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
+public class MysqlTest {
+
+	@Inject
+	private DataSource ds;
+
+	@Test
+	public void testDS() throws Exception {
+		Connection con = ds.getConnection();
+		System.out.println(con);
+		con.close();
+	}
+
+	@Test
+	public void DBTest() throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		Connection con = DriverManager.
+				getConnection("jdbc:mysql://192.168.0.7:3306/bit88?useSSL=false&serverTimezone=Asia/Seoul", "makemoney", "makemoney");
+		System.out.println(con);
+		con.close();
+
+	}
+	
+}
